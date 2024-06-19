e914d9d55148dc46ee5c09eda6f13573db4c73b3
HamaRigo <mohamed.bouallagui@esprit.tn>
2024-06-19 05:14:32 +0100

    CLEAN_UP

--- shared-module/src/main/java/dev/atb/models/Ocr.java
+++ shared-module/src/main/java/dev/atb/models/Ocr.java
@@ -12,6 +12,14 @@ public class Ocr {
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     private String id;  // Ensure this is String
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     @Column(nullable = false)
     private String typeDocument;
 
@@ -40,4 +48,6 @@ public class Ocr {
 
     public void setResultatsReconnaissance(String resultatsReconnaissance) {
     }
+
+
 }
