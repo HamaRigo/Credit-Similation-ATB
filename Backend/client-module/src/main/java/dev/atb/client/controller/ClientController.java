e9ad6fdf36a11afecf22bb628a90944fee5dbfc5
HamaRigo <mohamed.bouallagui@esprit.tn>
2025-01-05 17:59:54 +0100

    Add backend user module

--- Backend/client-module/src/main/java/dev/atb/client/controller/ClientController.java
+++ Backend/client-module/src/main/java/dev/atb/client/controller/ClientController.java
@@ -2,11 +2,7 @@ package dev.atb.client.controller;
 
 import dev.atb.client.service.ClientService;
 import dev.atb.dto.ClientDTO;
-import dev.atb.dto.RoleDTO;
-import dev.atb.dto.UserDTO;
 import dev.atb.models.Client;
-import dev.atb.models.Role;
-import dev.atb.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -73,96 +69,6 @@ public class ClientController {
             return new ResponseEntity<>(HttpStatus.OK); // Client exists
         } catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Client does not exist
-        }
-    }
-
-    /*****************************USERS************************************/
-    @GetMapping("/users")
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        return new ResponseEntity<>(clientService.getAllUsers(), HttpStatus.OK);
-    }
-
-    @GetMapping("/users/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable final Long id) {
-        try {
-            return new ResponseEntity<>(clientService.getUserById(id), HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/users")
-    public ResponseEntity<UserDTO> createUser(@RequestBody final User user) {
-        try {
-            return new ResponseEntity<>(clientService.createUser(user), HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable final Long id) {
-        try {
-            clientService.deleteUser(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PutMapping("/users")
-    public ResponseEntity<UserDTO> updateUser(@RequestBody final User user) {
-        try {
-            return new ResponseEntity<>(clientService.updateUser(user), HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /*****************************ROLES************************************/
-    @GetMapping("/roles")
-    public ResponseEntity<List<RoleDTO>> getAllRoles() {
-        return new ResponseEntity<>(clientService.getAllRoles(), HttpStatus.OK);
-    }
-
-    @GetMapping("/roles/{id}")
-    public ResponseEntity<?> getRoleById(@PathVariable final Long id) {
-        try {
-            return new ResponseEntity<>(clientService.getRoleById(id), HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/roles")
-    public ResponseEntity<RoleDTO> createRole(@RequestBody final Role role) {
-        try {
-            return new ResponseEntity<>(clientService.createRole(role), HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PutMapping("/roles")
-    public ResponseEntity<RoleDTO> updateRole(@RequestBody final Role role) {
-        try {
-            return new ResponseEntity<>(clientService.updateRole(role), HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/roles/{id}")
-    public ResponseEntity<Void> deleteRole(@PathVariable final Long id) {
-        try {
-            clientService.deleteRole(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
--- Backend/client-module/src/main/java/dev/atb/client/service/ClientService.java
+++ Backend/client-module/src/main/java/dev/atb/client/service/ClientService.java
@@ -1,15 +1,9 @@
 package dev.atb.client.service;
 
 import dev.atb.dto.ClientDTO;
-import dev.atb.dto.RoleDTO;
 import dev.atb.dto.ToDtoConverter;
-import dev.atb.dto.UserDTO;
 import dev.atb.models.Client;
-import dev.atb.models.Role;
-import dev.atb.models.User;
 import dev.atb.repo.ClientRepository;
-import dev.atb.repo.RoleRepository;
-import dev.atb.repo.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +16,6 @@ import java.util.stream.Collectors;
 public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
 
     private final Logger logger = LoggerFactory.getLogger(ClientService.class);
 
@@ -60,69 +48,5 @@ public class ClientService {
         Client client = clientRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Client not found"));
         clientRepository.delete(client);
-    }
-
-    /*****************************USERS************************************/
-    public List<UserDTO> getAllUsers() {
-        List<User> users = userRepository.findAllWithRoles();
-        return users.stream().map(ToDtoConverter::userToDto).collect(Collectors.toList());
-    }
-
-    public UserDTO getUserById(final Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        return ToDtoConverter.userToDto(user);
-    }
-
-    public UserDTO createUser(final User user) {
-        try {
-            return ToDtoConverter.userToDto(userRepository.save(user));
-        } catch (Exception e) {
-            throw new RuntimeException("Error saving user", e);
-        }
-    }
-
-    public UserDTO updateUser(final User user) {
-        userRepository.findById(user.getId())
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        return ToDtoConverter.userToDto(userRepository.save(user));
-    }
-
-    public void deleteUser(final Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        userRepository.delete(user);
-    }
-
-    /*****************************ROLES************************************/
-    public List<RoleDTO> getAllRoles() {
-        List<Role> roles = roleRepository.findAll();
-        return roles.stream().map(ToDtoConverter::roleToDto).collect(Collectors.toList());
-    }
-
-    public RoleDTO getRoleById(final Long id) {
-        Role role = roleRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Role not found"));
-        return ToDtoConverter.roleToDto(role);
-    }
-
-    public RoleDTO createRole(final Role role) {
-        try {
-            return ToDtoConverter.roleToDto(roleRepository.save(role));
-        } catch (Exception e) {
-            throw new RuntimeException("Error saving role", e);
-        }
-    }
-
-    public RoleDTO updateRole(final Role role) {
-        roleRepository.findById(role.getId())
-                .orElseThrow(() -> new RuntimeException("Role not found"));
-        return ToDtoConverter.roleToDto(roleRepository.save(role));
-    }
-
-    public void deleteRole(final Long id) {
-        Role role = roleRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Role not found"));
-        roleRepository.delete(role);
     }
 }
